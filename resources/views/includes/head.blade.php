59aec18d9f66bec64019191c2c5f165d6f443642
showalrm <ryanshowalter76@gmail.com>
2018-11-21 16:50:42 -0500

    move stylesheet and add favicon

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,75 +5,14 @@
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-<style>
-    body {
-        min-height: 100vh;
-        display: block;
-        position: relative;
-        padding-bottom: 80px;
-    }
+<!-- Custom CSS -->
+<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
 
-    a {
-        color: grey;
-    }
-
-    a:link {
-        text-decoration: none;
-    }
-
-    a:visited {
-        text-decoration: none;
-    }
-
-    a:hover {
-        color: grey;
-        text-decoration: underline;
-    }
-
-    a:active {
-        color: grey;
-        text-decoration: underline;
-    }
-
-    .profile {
-        margin: 0 auto;
-        margin-bottom: 2vh;
-        max-height: 50vh;
-        max-width: 100vw
-    }
-
-    .footer {
-        position: absolute;
-        bottom: 0;
-        width: 100%;
-        height: 80px;
-        line-height: 40px;
-        background-color: #f5f5f5;
-        margin-top: auto;
-    }
-
-    .footer-children {
-        display: flex;        
-        justify-content: center;
-        align-self: middle;
-    }
-
-    .fa {
-        padding-left: 20px;
-        padding-right: 20px;
-        font-size: 0px;
-        width: 30px;
-        text-align: center;
-        text-decoration: none!important;
-        border-radius: 50%;
-        color: gray;
-    }
-
-    .fa:hover {
-        color: black;
-    }
-</style>
-
+<!-- Social Icons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
 <title>@yield('title')</title>

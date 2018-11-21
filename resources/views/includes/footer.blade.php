eff8c74ba1b1d0e21ead83761295dcde19572294
showalrm <ryanshowalter76@gmail.com>
2018-11-21 17:29:51 -0500

    add social urls

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,10 +4,10 @@
             <p style="color: gray;">Copyright © Ryan Showalter 2018</p>
         </div>
         <div class="footer-children">
-            <a href="#" class="fa fa-instagram"></a>
-            <a href="#" class="fa fa-facebook"></a>
-            <a href="#" class="fa fa-github"></a>
-            <a href="#" class="fa fa-linkedin"></a>
+            <a href="https://www.instagram.com/ryan_showalter/" target="_blank" class="fa fa-instagram"></a>
+            <a href="https://www.facebook.com/ryan.showalter.944" target="_blank" class="fa fa-facebook"></a>
+            <a href="https://github.com/showalrm" target="_blank" class="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/r-showalter" target="_blank" class="fa fa-linkedin"></a>
         </div>
     </div>
 </div>

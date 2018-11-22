5e3406c3271a8418272562881936c029d89b9ddb
showalrm <ryanshowalter76@gmail.com>
2018-11-22 16:42:16 -0500

    add 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('master')
+
+@section('title', '404')
+
+@section('content')
+
+<div class="container" style="margin-top: 5vh;">
+    <h2>404</h2>
+    <p class="tail">Oops! The page you were looking for couldn't be found.</p>
+</div>
+
+@endsection
